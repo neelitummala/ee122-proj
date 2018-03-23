ccd15d122ded66302e42e1277009e0b738a85df9
Hall Chen <hall@hallchen.us>
2018-03-23 15:45:15 -0700

    need to finish Grid functions

--- Grid.py
+++ Grid.py
@@ -1,5 +1,7 @@
 from Point import *
 from Node import *
+
+import random
 import numpy as np
 
 class Grid:
@@ -8,6 +10,22 @@ class Grid:
         # creates a square grid of dimensions size x size
         # grid is a 2D numpy array
         
-        self.__size = size
+        self.__gridsize = size
         self.__grid = np.zeros((size,size), dtype=Node)
         
+    # TODO: complete this fn
+    def populate(self, swarm_size, seed=None):
+        # populates the grid with a swarm of size swarm_size
+        return None
+        
+    # TODO: need to generate n points without repeat
+    def getRandomCoordinates(self, n, seed):
+        x = np.zeros(n)
+        y = np.zeros(n)
+        
+        random.seed(seed)
+        
+        for i in range(n):
+            x[i] = random.randrange(self.__gridsize)
+        
+        return x
