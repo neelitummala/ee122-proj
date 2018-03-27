214c91eeaacb9e18796c94abf7f43e42d604c011
Hall Chen <hall@hallchen.us>
2018-03-27 10:39:05 -0700

    implemented find Neighbors

--- Grid.py
+++ Grid.py
@@ -1,19 +1,25 @@
 from Point import *
 from Node import *
 
-import random
+import random, math
 import numpy as np
 
 class Grid:
     
+    # arbitrary definition of Rx/Tx reachable radius
+    __radioRadius = 5
+    
     def __init__(self, size):
         # creates a square grid of dimensions size x size
         # grid is a 2D numpy array
+        # unpopulated points in the Grid denoted by 0
         
         self.__gridsize = size
         self.__grid = np.zeros((size,size), dtype=Node)
+        self.__devices = []
+        self.__allNeighbors = {}
         self.populate(size, 0)
-        
+        self.findNeighbors()
         
     # TODO: defined as average number of immediately adjacent neighbors that each node
     #       in the swarm can communicate with
@@ -22,8 +28,44 @@ class Grid:
     
     # generates a list of neighbors for each Node in the grid
     def findNeighbors(self):
-        allNeighbors = []
+        
+        # helper function for fast localized neighbor search
+        # returns [upperLeftX, upperLeftY, lowerRightX, lowerRightY]
+        def getRadiusCorners(point):
+            x = point.getX()
+            y = point.getY()
+            ulx = (x - self.__radioRadius)
+            uly = (y  - self.__radioRadius)
+            lrx = (x + self.__radioRadius)
+            lry = (y + self.__radioRadius)
+            if ulx < 0:
+                ulx = 0
+            if uly < 0:
+                uly = 0
+            if lrx > (self.__gridsize - 1):
+                lrx = (self.__gridsize - 1)
+            if lry > (self.__gridsize - 1):
+                lry = (self.__gridsize - 1)
+            return [ulx, uly, lrx, lry]
+            
+        for d in self.__devices:
+            neighbors = []
+            ulx, uly, lrx, lry = getRadiusCorners(d.getCoordinate())
+            for x in range(ulx,lrx+1):
+                for y in range(uly,lry+1):
+                    n = self.__grid[x,y]
+                    if (type(n) == Node) and (n != d) and (n.distanceToNode(d) < self.__radioRadius):
+                        neighbors.append(n)
+                        
+            self.__allNeighbors[d] = neighbors
+                        
         return None
+    
+    def getNeighborsDict(self):
+        return self.__allNeighbors
+    
+    def getGrid(self):
+        return self.__grid
     
     # populates the grid with a swarm of size swarm_size
     def populate(self, swarm_size, seed=None):
@@ -35,6 +77,7 @@ class Grid:
             c = randomCoordinates.pop()
             n = Node(i, c)
             self.__grid[c.getX(), c.getY()] = n
+            self.__devices.append(n)
         
         return None
         
