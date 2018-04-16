9c0e405638a6620a24bb1a9b30aa936952d86cd8
Neeli Tummala <neeli.tummala@berkeley.edu>
2018-04-16 09:47:19 -0700

    Added transmissions and get_p functions to simulations.py

--- simulation.py
+++ simulation.py
@@ -1,21 +1,68 @@
 # File for running simulation
 
 import numpy as np
+import Grid 
 
-def create_timeslots(num_slots, num_nodes, prob):
-	# num_slots is number of slots simulation should run for
-	# num_nodes is number of nodes in the network
-	# prob is array of probabilities of successful transmission for each node
-	# returns an array of time slots with an array of the nodes transmitting in each slot
+def get_p(grid, node):
+    """
+        Probability of successful transmission for a given node.
 
-	time_slots = [[] for i in range(num_slots)]
-	for node in np.arange(num_nodes):
-		transmissions = np.random.choice(2, num_slots, p=[1-prob[node], prob[node]])
-		for j in np.nonzero(transmissions)[0]:
-			time_slots[j].append(node)
+        Parameters
+        ----------
+        grid: Grid object
+            the grid of the swarm
+        node: int
+            node we need the probability of transmission for
 
-	return time_slots
+        Returns
+        -------
+        :int
+            probability of successful transmission for a given node
+    """
+    # TODO: change probability of transmission based on number of neighbors
+    # for now return a constant
+    return 0.5
+
+def transmissions(grid):
+    """
+        Which nodes transmit in the current timeslot.
+
+        Parameters
+        ----------
+        grid: Grid object
+            the grid of the swarm
+
+        Returns
+        -------
+        :obj:list
+            list of nodes that will transmit in the current timeslot
+    """
+    numNodes = grid.__idCount
+    transmissions = []
+    for node in np.arange(numNodes):
+        p = get_p(grid, node)
+        if np.random.choice(2, p=[1-p, p]):
+            transmissions.append(node)
+    return transmissions
+
+def run_simulation():
+    """
+        Which nodes transmit in the current timeslot.
+
+        Parameters
+        ----------
+        grid: Grid object
+            the grid of the swarm
+
+        Returns
+        -------
+        :obj:list
+            list of nodes that will transmit in the current timeslot
+    """
+    grid = Grid(10)
+    
+    return
 
 if __name__ == "__main__":
-	time_slots = create_timeslots(100, 5, [0.1, 0.2, 0.8, 0.9, 0.01])
-	print(time_slots)
+	test_grid = Grid(10)
+	print(test_grid)
